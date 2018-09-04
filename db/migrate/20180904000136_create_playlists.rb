04a8dc6a73c5840d5f7454f591db2ab66cfc8413
Timothy Ting <timothy@tune.com>
2018-09-03 17:50:41 -0700

    First commit

---
+++ db/migrate/20180904000136_create_playlists.rb
@@ -0,0 +1,11 @@
+class CreatePlaylists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :playlists do |t|
+      t.string :owner_id
+      t.string :song_ids, array: true
+      t.string :shared_with, array: true
+
+      t.timestamps
+    end
+  end
+end
